15dbf79c0520075ae39f61ba626463e4f7b039ae
Matt Cui <mattcui@gmail.com>
2016-10-24 19:30:43 +0800

    Added vendor main and routers

--- cmd/main.go
+++ cmd/main.go
@@ -1,150 +1,23 @@
-package cmd
+package main
 
 import (
-	"os"
-	"fmt"
-	"io/ioutil"
-	"crypto/tls"
-	"crypto/x509"
-	"flag"
-	"errors"
-	"database/sql"
+	// WARNING!
+	// Change this to a fully-qualified import path
+	// once you place this file into your project.
+	// For example,
+	//
+	//    sw "github.com/myname/myrepo/go"
+	//
+	"log"
+	"net/http"
 
-	"code.cloudfoundry.org/cflager"
-	"code.cloudfoundry.org/lager"
-	"code.cloudfoundry.org/clock"
-	"github.com/vm-pool-server/db"
-	"github.com/vm-pool-server/db/sqldb"
-	"github.com/go-sql-driver/mysql"
-)
-
-var accessLogPath = flag.String(
-	"accessLogPath",
-	"",
-	"Location of the access log",
-)
-
-var databaseConnectionString = flag.String(
-	"databaseConnectionString",
-	"",
-	"SQL database connection string",
-)
-
-var maxDatabaseConnections = flag.Int(
-	"maxDatabaseConnections",
-	200,
-	"Max numbers of SQL database connections",
-)
-
-var databaseDriver = flag.String(
-	"databaseDriver",
-	"mysql",
-	"SQL database driver name",
-)
-
-var sqlCACertFile = flag.String(
-	"sqlCACertFile",
-	"",
-	"SQL database client cert, if supplied, require TLS to SQL",
+	sw "github.com/jianqiu/vm-pool-server"
 )
 
 func main() {
-	flag.Parse()
+	log.Printf("Server started")
 
-	logger, _ := cflager.New("virtual-guest-pool-server")
-	logger.Info("starting")
+	router := sw.NewRouter()
 
-	clock := clock.NewClock()
-
-	var activeDB db.DB
-	var sqlDB *sqldb.SQLDB
-	var sqlConn *sql.DB
-
-	if *databaseDriver != "" && *databaseConnectionString != "" {
-		var err error
-		connectionString := appendSSLConnectionStringParam(logger, *databaseDriver, *databaseConnectionString, *sqlCACertFile)
-
-		sqlConn, err = sql.Open(*databaseDriver, connectionString)
-		if err != nil {
-			logger.Fatal("failed-to-open-sql", err)
-		}
-		defer sqlConn.Close()
-		sqlConn.SetMaxOpenConns(*maxDatabaseConnections)
-		sqlConn.SetMaxIdleConns(*maxDatabaseConnections)
-
-		err = sqlConn.Ping()
-		if err != nil {
-			logger.Fatal("sql-failed-to-connect", err)
-		}
-
-		sqlDB = sqldb.NewSQLDB(sqlConn,  clock, *databaseDriver)
-		err = sqlDB.CreateConfigurationsTable(logger)
-		if err != nil {
-			logger.Fatal("sql-failed-create-configurations-table", err)
-		}
-		activeDB = sqlDB
-	}
-
-	if activeDB == nil {
-		logger.Fatal("no-database-configured", errors.New("no database configured"))
-	}
-
-
-	migrationsDone := make(chan struct{})
-
-	migrationManager := migration.NewManager(
-		logger,
-		sqlDB,
-		sqlConn,
-		migrations.Migrations,
-		migrationsDone,
-		clock,
-		*databaseDriver,
-	)
-
-	var accessLogger lager.Logger
-	if *accessLogPath != "" {
-		accessLogger = lager.NewLogger("bbs-access")
-		file, err := os.OpenFile(*accessLogPath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-		if err != nil {
-			logger.Error("invalid-access-log-path", err, lager.Data{"access-log-path": *accessLogPath})
-			os.Exit(1)
-		}
-		accessLogger.RegisterSink(lager.NewWriterSink(file, lager.INFO))
-	}
-
-
-}
-
-func appendSSLConnectionStringParam(logger lager.Logger, driverName, databaseConnectionString, sqlCACertFile string) string {
-	switch driverName {
-	case "mysql":
-		if sqlCACertFile != "" {
-			certBytes, err := ioutil.ReadFile(sqlCACertFile)
-			if err != nil {
-				logger.Fatal("failed-to-read-sql-ca-file", err)
-			}
-
-			caCertPool := x509.NewCertPool()
-			if ok := caCertPool.AppendCertsFromPEM(certBytes); !ok {
-				logger.Fatal("failed-to-parse-sql-ca", err)
-			}
-
-			tlsConfig := &tls.Config{
-				InsecureSkipVerify: false,
-				RootCAs:            caCertPool,
-			}
-
-			mysql.RegisterTLSConfig("bbs-tls", tlsConfig)
-			databaseConnectionString = fmt.Sprintf("%s?tls=bbs-tls", databaseConnectionString)
-		}
-	case "postgres":
-		if sqlCACertFile == "" {
-			databaseConnectionString = fmt.Sprintf("%s?sslmode=disable", databaseConnectionString)
-		} else {
-			databaseConnectionString = fmt.Sprintf("%s?sslmode=verify-ca&sslrootcert=%s", databaseConnectionString, sqlCACertFile)
-		}
-	}
-
-	return databaseConnectionString
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
--- controllers/virtual_guest_controller.go
+++ controllers/virtual_guest_controller.go
@@ -1,24 +1,24 @@
 package controllers
 
 import (
-	. "github.com/vm-pool-server"
-	"github.com/vm-pool-server/db"
-	"github.com/vm-pool-server/models"
 	"code.cloudfoundry.org/lager"
+	"github.com/jianqiu/vm-pool-server"
+	"github.com/jianqiu/vm-pool-server/db"
+	"github.com/jianqiu/vm-pool-server/models"
 )
 
 type VirtualGuestController struct {
-	db                   db.VirtualGuestDB
-	serviceClient        ServiceClient
+	db            db.VirtualGuestDB
+	serviceClient ServiceClient
 }
 
 func NewVirtualGuestController(
-db db.VirtualGuestDB,
-serviceClient ServiceClient,
+	db db.VirtualGuestDB,
+	serviceClient ServiceClient,
 ) *VirtualGuestController {
 	return &VirtualGuestController{
-		db:                   db,
-		serviceClient:        serviceClient,
+		db:            db,
+		serviceClient: serviceClient,
 	}
 }
 
@@ -28,13 +28,13 @@ func (h *VirtualGuestController) VirtualGuests(logger lager.Logger, filter model
 	return h.db.VirtualGuests(logger, filter)
 }
 
-func (h *VirtualGuestController) VirtualGuestByCid(logger lager.Logger, cid string) (*models.VirtualGuest, error) {
+func (h *VirtualGuestController) RequestVirtualGuestByCID(logger lager.Logger, cid string) (*models.VirtualGuest, error) {
 	logger = logger.Session("virutal-guest-by-guid")
 
 	return h.db.VirtualGuestByCID(logger, cid)
 }
 
-func (h *VirtualGuestController) RemoveVirtualGuestFromPool(logger lager.Logger, cid int32) error {
+func (h *VirtualGuestController) ReturnVirtualGuestIntoPool(logger lager.Logger, cid int32) error {
 	logger = logger.Session("delete-virtual-guest")
 
 	return h.db.DeleteVirtualGuestFromPool(logger, cid)
